9ee923b4f153e830fb34cbb02670a0c0ce4f3552
khoapn-1940 <pham.nguyen.khoa@sun-asterisk.com>
2019-06-04 17:07:44 +0700

    Chapter 6: Modeling users

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,8 @@ require "test_helper"
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-    assert_equal full_title, I18n.t("application.applicationtittle")
-    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
+    assert_equal full_title(""), I18n.t("application.applicationtittle")
+    assert_equal full_title("Help | "),
+      I18n.t("application.applicationc5helptitle")
   end
 end
