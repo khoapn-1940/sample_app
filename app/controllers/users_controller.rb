e7348869e9ad4871eb4cd901495250d7483e1a92
khoapn-1940 <pham.nguyen.khoa@sun-asterisk.com>
2019-06-05 17:11:38 +0700

    Chapter 7: Sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by_id params[:id]
+    return if @user
+    render html: (t "chap6.nouser")
   end
 
   def new
@@ -8,18 +10,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new user_params
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = t "chap6.success"
       redirect_to @user
     else
-      render "new"
+      render :new
     end
   end
 
   private
+
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-      :password_confirmation)
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
   end
 end
