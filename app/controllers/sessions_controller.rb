45ea29b27968a4bf1c4a0667b6ed3ed36f9f5a5a
khoapn-1940 <pham.nguyen.khoa@sun-asterisk.com>
2019-06-06 17:10:28 +0700

    Chapter 9: Advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,28 @@
 class SessionsController < ApplicationController
   def new; end
 
+  def authen_success user
+    log_in user
+    params[:session][:remember_me] == Settings.m ? remember(user) : forget(user)
+    redirect_to user
+  end
+
+  def authen_fail
+    flash.now[:danger] = t "chap7.invalidlogin"
+    render :new
+  end
+
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user
-      if user.authenticate(params[:session][:password])
-        log_in user
-        redirect_to user
-      end
+    user = User.find_by email: params[:session][:email].downcase
+    if user&.authenticate(params[:session][:password])
+      authen_success user
     else
-      flash.now[:danger] = t "chap7.invalidlogin"
-      render :new
+      authen_fail
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
